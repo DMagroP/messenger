e074c6a882682d9734524b0ba366444cdbc46a5a
David Magro <david.magro@ex-gr.com>
2018-09-25 10:34:53 +0200

    8. Componentes de BV para formularios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,58 +11,39 @@
                         Por favor, ingresa tus datos.
                     </b-alert>
 
-                    <b-form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                    <b-form method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <b-form-group id="exampleInputGroup1"
-                                        label="Correo Electrónico"
-                                        label-for="email"
-                                        description="Nunca compartiremos tu correo. Está seguro con nosotros.">
-                            <b-form-input id="email"
-                                        type="email"
-                                        name="email"
-                                        value="{{ old('email') }}"
-                                        required
-                                        autofocus
-                                        placeholder="example@messenger.com">
+                        <b-form-group label="Correo Electrónico" label-for="email"
+                            description="Nunca compartiremos tu correo. Está seguro con nosotros.">
+                            
+                            <b-form-input type="email"
+                                id="email" name="email"
+                                value="{{ old('email') }}" required autofocus
+                                placeholder="example@messenger.com">
+                            </b-form-input>
+                        </b-form-group>
+                        
+                        <b-form-group label="Contraseña" label-for="password">
+                            
+                            <b-form-input type="password"
+                                id="password" name="password"
+                                value="{{ old('password') }}" required>
                             </b-form-input>
                         </b-form-group>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                        <b-form-group>
+                            <b-form-checkbox name="remember" {{ old('remember') ? 'checked="true"' : '' }}>
+                                Recordar sesión
+                            </b-form-checkbox>
+                        </b-form-group>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
+                        <b-button type="submit" variant="primary">
+                            Ingresar
+                        </b-button>
+                        <b-button href="{{ route('password.request') }}" variant="link">
+                            ¿Olvidaste tu contraseña?
+                        </b-button>
                     </b-form>
                 </b-card>
             </b-card-group>
